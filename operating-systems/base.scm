093e97fd5b4d801a921d84c0a2608592b5b88748
Jaden Nola <proto@jadestar.dev>
2026-06-10 15:30:33 -0700

    feat: add nonguix channel to base operating-systems

--- operating-systems/base.scm
+++ operating-systems/base.scm
@@ -1,6 +1,20 @@
 (define-module (operating-systems base)
   #:use-module (gnu system)
+  #:use-module (gnu packages package-management)
+  #:use-module (guix channels)
+
   #:export (base))
+
+(define system-channels
+  (append
+    (list
+      (channel
+        (name 'nonguix)
+        (url "https://gitlab.com/nonguix/nonguix")
+        (introduction
+          (make-channel-introduction "897c1a470da759236cc11798f4e0a5f7d4d59fbc")
+          (openpgp-fingerprint "2A39 3FFF 68F4 EF7A 3D29  12AF 6F51 20A0 22FB B2D5"))))
+    %default-channels))
 
 (define base
   (operating-system
@@ -10,4 +24,11 @@
     (firmware '())
     (file-systems '())
     (bootloader #f)
-  ))
+    (services
+      (modify-services
+        %base-services
+        (guix-service-type
+          config => (guix-configuration
+                      (inherit config)
+                      (channels system-channels)
+                      (guix (guix-for-channels system-channels))))))))
